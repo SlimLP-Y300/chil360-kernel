bc896b3c64faab2c82ba4dcff246d09f2d176e61
Pushkar Joshi <pushkarj@codeaurora.org>
2012-09-07 14:22:58 -0700

    xtracing: ftrace events for user faults and undefined instructions
    New ftrace events (user_fault and undef_instr) for data, prefetch
    or undefined instruction aborts. The new ftrace events are under
    events/exception.
    
    Change-Id: Iea328b71a1f623861cac9b45d858c3bbe09e1b82
    Signed-off-by: Pushkar Joshi <pushkarj@codeaurora.org>

--- arch/arm/mm/fault.c
+++ arch/arm/mm/fault.c
@@ -176,6 +176,8 @@ __do_user_fault(struct task_struct *tsk, unsigned long addr,
 {
 	struct siginfo si;
 
+	trace_user_fault(tsk, addr, fsr);
+
 #ifdef CONFIG_DEBUG_USER
 	if (((user_debug & UDBG_SEGV) && (sig == SIGSEGV)) ||
 	    ((user_debug & UDBG_BUS)  && (sig == SIGBUS))) {
