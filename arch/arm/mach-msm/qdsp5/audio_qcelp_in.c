3c6e2c94f3015fd711a2e41cec1ff9fa087f9539
Mitchel Humpherys <mitchelh@codeaurora.org>
2012-09-12 14:42:50 -0700

    ion: change ion kernel map function to not take flags  argument
    Buffer flags are going to be specified at allocation time rather than
    map time. This removes the flags argument from the ion kernel map
    function.
    
    Change-Id: Ib983ecd0dcd7befb36287ae7037c71d4ca475f90
    Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>
    
    Conflicts:
    	sound/soc/msm/qdsp6v2/q6asm.c
    	sound/soc/msm/qdsp6v2/q6voice.c

--- arch/arm/mach-msm/qdsp5/audio_qcelp_in.c
+++ arch/arm/mach-msm/qdsp5/audio_qcelp_in.c
@@ -1341,7 +1341,7 @@ static int audqcelp_in_open(struct inode *inode, struct file *file)
 		goto output_buff_get_flags_error;
 	}
 
-	audio->map_v_read = ion_map_kernel(client, handle, ionflag);
+	audio->map_v_read = ion_map_kernel(client, handle);
 	if (IS_ERR(audio->map_v_read)) {
 		MM_ERR("could not map read buffers,freeing instance 0x%08x\n",
 				(int)audio);
@@ -1386,8 +1386,7 @@ static int audqcelp_in_open(struct inode *inode, struct file *file)
 			goto input_buff_get_flags_error;
 		}
 
-		audio->map_v_write = ion_map_kernel(client,
-			handle, ionflag);
+		audio->map_v_write = ion_map_kernel(client, handle);
 		if (IS_ERR(audio->map_v_write)) {
 			MM_ERR("could not map write buffers\n");
 			rc = -ENOMEM;
