ada1a148de44cc17a47afcb559fa475efe36a441
Sebastian Hesselbarth <sebastian.hesselbarth@gmail.com>
2013-08-01 14:10:46 +0200

    drivers: i2c-core: *squashing minor updates*
    i2c: add sanity check to i2c_put_adapter
    
    i2c_put_adapter dereferences i2c_adapter pointer passed without check
    for NULL. This adds a check for non-NULL pointer to allow i2c_put_adapter
    called with NULL and behave the same way i2c_release_client does already.
    It allows to simplify drivers where you need to release the adapter
    during probe failures.
    
    Signed-off-by: Sebastian Hesselbarth <sebastian.hesselbarth@gmail.com>
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>
    
    i2c: Improve logging on failure to probe for ->class devices
    
    While writing the i2c-imc driver, I noticed that the warning message
    when the i2c core can't figure out how to probe is mostly useless.
    This trivial patch improves it.
    
    Signed-off-by: Andy Lutomirski <luto@amacapital.net>
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>
    
    i2c: Fall back to emulated SMBus if the operation isn't supported natively
    
    Adapter drivers might support only a subset of the SMBus operations
    natively. Those drivers currently have to manually emulate unsupported
    operations using I2C.
    
    Make the i2c_smbus_xfer() function fall back to
    i2c_smbus_xfer_emulated() when the adapter's .smbus_xfer() operation
    returns -EOPNOTSUPP, like it already does when the .smbus_xfer()
    operation isn't available at all.
    
    [JD: Minor optimization.]
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Jean Delvare <khali@linux-fr.org>

--- drivers/i2c/i2c-core.c
+++ drivers/i2c/i2c-core.c
@@ -1472,7 +1472,8 @@ static int i2c_default_probe(struct i2c_adapter *adap, unsigned short addr)
 		err = i2c_smbus_xfer(adap, addr, 0, I2C_SMBUS_READ, 0,
 				     I2C_SMBUS_BYTE, &dummy);
 	else {
-		dev_warn(&adap->dev, "No suitable probing method supported\n");
+		dev_warn(&adap->dev, "No suitable probing method supported for address 0x%02X\n",
+			 addr);
 		err = -EOPNOTSUPP;
 	}
 
@@ -1632,7 +1633,8 @@ EXPORT_SYMBOL(i2c_get_adapter);
 
 void i2c_put_adapter(struct i2c_adapter *adap)
 {
-	module_put(adap->owner);
+	if (adap)
+		module_put(adap->owner);
 }
 EXPORT_SYMBOL(i2c_put_adapter);
 
@@ -2132,11 +2134,17 @@ s32 i2c_smbus_xfer(struct i2c_adapter *adapter, u16 addr, unsigned short flags,
 				break;
 		}
 		i2c_unlock_adapter(adapter);
-	} else
-		res = i2c_smbus_xfer_emulated(adapter, addr, flags, read_write,
-					      command, protocol, data);
 
-	return res;
+		if (res != -EOPNOTSUPP || !adapter->algo->master_xfer)
+			return res;
+		/*
+		 * Fall back to i2c_smbus_xfer_emulated if the adapter doesn't
+		 * implement native support for the SMBus operation.
+		 */
+	}
+
+	return i2c_smbus_xfer_emulated(adapter, addr, flags, read_write,
+				       command, protocol, data);
 }
 EXPORT_SYMBOL(i2c_smbus_xfer);
 
