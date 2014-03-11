aa3be73331445f79cd4c88ba2638ef81cccc24b9
Kyle Harrison <khwebmail@gmail.com>
2014-03-11 17:28:44 +0000

    Overclock gpu to 320Mhz and add additional freq step Thanks Ceastel!

--- arch/arm/mach-msm/devices-msm7x27a.c
+++ arch/arm/mach-msm/devices-msm7x27a.c
@@ -929,14 +929,19 @@ void __init msm8x25_kgsl_3d0_init(void)
 	if (cpu_is_msm8625()) {
 		kgsl_3d0_pdata.idle_timeout = HZ/5;
 		kgsl_3d0_pdata.strtstp_sleepwake = false;
-
-		if (SOCINFO_VERSION_MAJOR(socinfo_get_version()) >= 2)
-			/* 8x25 v2.0 & above supports a higher GPU frequency */
-			kgsl_3d0_pdata.pwrlevel[0].gpu_freq = 320000000;
-		else
-			kgsl_3d0_pdata.pwrlevel[0].gpu_freq = 300000000;
-
-		kgsl_3d0_pdata.pwrlevel[0].bus_freq = 200000000;
+		kgsl_3d0_pdata.num_levels = 4;
+ 
+                kgsl_3d0_pdata.pwrlevel[0].gpu_freq = 320000000;
+                kgsl_3d0_pdata.pwrlevel[0].bus_freq = 200000000;
+ 
+                kgsl_3d0_pdata.pwrlevel[1].gpu_freq = 245760000;
+                kgsl_3d0_pdata.pwrlevel[1].bus_freq = 200000000;
+ 
+                kgsl_3d0_pdata.pwrlevel[2].gpu_freq = 192000000;
+                kgsl_3d0_pdata.pwrlevel[2].bus_freq = 160000000;
+ 
+		kgsl_3d0_pdata.pwrlevel[3].gpu_freq = 96000000;
+		kgsl_3d0_pdata.pwrlevel[3].bus_freq = 0;
 	}
 }
 
