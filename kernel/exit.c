7af7ed3ec972a47d37fffdf91469f35fa85c32f3
moddingg33k <moddingg33k@t-online.de>
2014-07-13 20:21:10 +0200

    kernel/exit.c: remove Huawei kernel log spam
    Signed-off-by: moddingg33k <moddingg33k@t-online.de>

--- kernel/exit.c
+++ kernel/exit.c
@@ -987,12 +987,6 @@ void do_exit(long code)
 		tty_audit_exit();
 	audit_free(tsk);
 
-#ifdef CONFIG_HUAWEI_KERNEL
-	if( code != 0 ){
-		printk("%s: exit code=%lu\n", __func__, code);
-	}
-#endif
-
 	tsk->exit_code = code;
 	taskstats_exit(tsk, group_dead);
 
