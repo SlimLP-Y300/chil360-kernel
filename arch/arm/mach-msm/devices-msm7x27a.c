714136b24f345b15ee17f3847eefb46a2f95a739
Kyle Harrison <khwebmail@gmail.com>
2015-02-03 22:47:39 +0000

    Overclock gpu to 320Mhz for msm8x25

--- arch/arm/mach-msm/devices-msm7x27a.c
+++ arch/arm/mach-msm/devices-msm7x27a.c
@@ -938,11 +938,9 @@ void __init msm8x25_kgsl_3d0_init(void)
 		kgsl_3d0_pdata.idle_timeout = HZ/5;
 		kgsl_3d0_pdata.strtstp_sleepwake = false;
 
-		if (SOCINFO_VERSION_MAJOR(socinfo_get_version()) >= 2) {
-			/* 8x25 v2.0 & above supports a higher GPU frequency */
-			kgsl_3d0_pdata.pwrlevel[0].gpu_freq = 320000000;
-			kgsl_3d0_pdata.pwrlevel[0].bus_freq = 200000000;
-		}
+		/* 8x25 supports a higher GPU frequency */
+		kgsl_3d0_pdata.pwrlevel[0].gpu_freq = 320000000;
+		kgsl_3d0_pdata.pwrlevel[0].bus_freq = 200000000;
 	}
 }
 
