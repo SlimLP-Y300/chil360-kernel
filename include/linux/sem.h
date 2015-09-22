7b9a0093153ea6a5be0175ba6aaa4efac477ccce
srfarias <ericfari15@gmail.com>
2015-09-22 14:14:24 -0300

    More tweaks

--- include/linux/sem.h
+++ include/linux/sem.h
@@ -63,10 +63,10 @@ struct  seminfo {
 	int semaem;
 };
 
-#define SEMMNI  128             /* <= IPCMNI  max # of semaphore identifiers */
-#define SEMMSL  250             /* <= 8 000 max num of semaphores per id */
+#define SEMMNI  256             /* <= IPCMNI  max # of semaphore identifiers */
+#define SEMMSL  350             /* <= 8 000 max num of semaphores per id */
 #define SEMMNS  (SEMMNI*SEMMSL) /* <= INT_MAX max # of semaphores in system */
-#define SEMOPM  32	        /* <= 1 000 max num of ops per semop call */
+#define SEMOPM  64	        /* <= 1 000 max num of ops per semop call */
 #define SEMVMX  32767           /* <= 32767 semaphore maximum value */
 #define SEMAEM  SEMVMX          /* adjust on exit max value */
 
