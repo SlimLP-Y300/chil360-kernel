7951d9a411602f0676a761cc92c5dcbbfe7e6acb
Kyle Harrison <khwebmail@gmail.com>
2016-03-16 19:32:40 +0000

    CHROMIUM: arch/arm: add asm/syscall.h
    (I will post this upstream after the 3.5 merge window)
    
    Provide an ARM implementation for asm-generic/syscall.h.
    This is a pre-requisite for CONFIG_HAVE_ARCH_TRACEHOOK and
    CONFIG_HAVE_ARCH_SECCOMP_FILTER.  The latter is the forcing
    function for this patch.
    
    Change-Id: Idc5fa7b72691ec9d75418849733633df33482e53
    Signed-off-by: Will Drewry <wad@chromium.org>
    
    BUG=chromium-os:27878
    TEST=compiles for arm. Need to test on a live machine.
    
    Change-Id: I7b911b51085424aedd2beaf40683c3348b6cede1
    Reviewed-on: https://gerrit.chromium.org/gerrit/21375
    Reviewed-by: Will Drewry <wad@chromium.org>
    Tested-by: Will Drewry <wad@chromium.org>
    Signed-off-by: Sasha Levitskiy <sanek@google.com>

--- arch/arm/include/asm/syscall.h
+++ arch/arm/include/asm/syscall.h
@@ -1,18 +1,24 @@
 /*
  * Access to user system call parameters and results
  *
+ * Copyright (C) 2012 The Chromium OS Authors <chromium-os-dev@chromium.org>
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU General Public License v.2.
+ *
  * See asm-generic/syscall.h for descriptions of what we must do here.
  */
-
 #ifndef _ASM_ARM_SYSCALL_H
 #define _ASM_ARM_SYSCALL_H
 
+#include <linux/audit.h> /* for AUDIT_ARCH_* */
+#include <linux/elf.h> /* for ELF_EM */
+#include <linux/sched.h>
+#include <linux/thread_info.h> /* for task_thread_info */
 #include <linux/err.h>
 
-extern const unsigned long sys_call_table[];
-
-static inline int syscall_get_nr(struct task_struct *task,
-				 struct pt_regs *regs)
+static inline int syscall_get_nr(struct task_struct *task, struct pt_regs *regs)
 {
 	return task_thread_info(task)->syscall;
 }
@@ -40,33 +46,16 @@ static inline void syscall_set_return_value(struct task_struct *task,
 					    struct pt_regs *regs,
 					    int error, long val)
 {
-	regs->ARM_r0 = (long) error ? error : val;
+	regs->ARM_r0 = (long) error ?: val;
 }
-
-#define SYSCALL_MAX_ARGS 7
 
 static inline void syscall_get_arguments(struct task_struct *task,
 					 struct pt_regs *regs,
 					 unsigned int i, unsigned int n,
 					 unsigned long *args)
 {
-	if (i + n > SYSCALL_MAX_ARGS) {
-		unsigned long *args_bad = args + SYSCALL_MAX_ARGS - i;
-		unsigned int n_bad = n + i - SYSCALL_MAX_ARGS;
-		pr_warning("%s called with max args %d, handling only %d\n",
-			   __func__, i + n, SYSCALL_MAX_ARGS);
-		memset(args_bad, 0, n_bad * sizeof(args[0]));
-		n = SYSCALL_MAX_ARGS - i;
-	}
-
-	if (i == 0) {
-		args[0] = regs->ARM_ORIG_r0;
-		args++;
-		i++;
-		n--;
-	}
-
-	memcpy(args, &regs->ARM_r0 + i, n * sizeof(args[0]));
+	BUG_ON(i + n > 6);
+	memcpy(args, &regs->ARM_r1 + i, n * sizeof(args[0]));
 }
 
 static inline void syscall_set_arguments(struct task_struct *task,
@@ -74,20 +63,18 @@ static inline void syscall_set_arguments(struct task_struct *task,
 					 unsigned int i, unsigned int n,
 					 const unsigned long *args)
 {
-	if (i + n > SYSCALL_MAX_ARGS) {
-		pr_warning("%s called with max args %d, handling only %d\n",
-			   __func__, i + n, SYSCALL_MAX_ARGS);
-		n = SYSCALL_MAX_ARGS - i;
-	}
-
-	if (i == 0) {
-		regs->ARM_ORIG_r0 = args[0];
-		args++;
-		i++;
-		n--;
-	}
-
-	memcpy(&regs->ARM_r0 + i, args, n * sizeof(args[0]));
+	BUG_ON(i + n > 6);
+	memcpy(&regs->ARM_r1 + i, args, n * sizeof(args[0]));
 }
 
-#endif /* _ASM_ARM_SYSCALL_H */
+static inline int syscall_get_arch(struct task_struct *task,
+				    struct pt_regs *regs)
+{
+	/* ARM tasks don't change audit architectures on the fly. */
+#ifdef __ARMEB__
+	return AUDIT_ARCH_ARMEB;
+#else
+	return AUDIT_ARCH_ARM;
+#endif
+}
+#endif	/* _ASM_ARM_SYSCALL_H */
